da3fdb0fcfa8b143f5335b9815206399c1b9241c
Caio Pumar <caiopumar@gmail.com>
2019-12-05 13:52:26 +0900

    Update oop.py

--- oop.py
+++ oop.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Dec  4 17:03:16 2019
 
-@author: z5g1327
-"""
-
-import os
 
 class NGramTokenizer(object):
     """
@@ -20,9 +14,10 @@ class NGramTokenizer(object):
         
         self.ngrams = ngrams
         self.skipgram = skipgram
-        self.ngram_index = dict()
+        self.ngram_dict = dict()
         
     def text_fitting(self,corpus,one_char=False):
+        
         """This function should receive as input the entire available corpus\
            so as to provice a representative relative importance of ngrams
            RETURNS: a dictionary mapping ngrams to ordered integers
@@ -36,30 +31,30 @@ class NGramTokenizer(object):
                     skip = self.skipgram
                 for char_index in range(0,len(document)-gram+1,skip):
                     current_gram = document[char_index:char_index+gram]
-                    if current_gram in self.ngram_index:
-                        self.ngram_index[current_gram] += 1
+                    if current_gram in self.ngram_dict:
+                        self.ngram_dict[current_gram] += 1
                     else:
-                        self.ngram_index[current_gram] = 1
+                        self.ngram_dict[current_gram] = 1
         # Filters characters that only appear once in corpus
         #if one_char:
         #    continue
         dict_list= list()
         ### Sorting the dictionary through a 'zipped' list
-        for (key,value) in self.ngram_index.items():
+        for (key,value) in self.ngram_dict.items():
             dict_list.append((key,value))
         dict_list = sorted(dict_list,key=lambda x: x[1],reverse=True)
         ### Refactors the dictionary with each value now corresponding to the order\
         #\  in which it is prevalent in the corpus. (e.g. space-> ' ' = 1)\
         ### OBS: Starts from 1, since 0 is a reserved value for padding sequences
         for i,pair in enumerate(dict_list):
-            self.ngram_index[pair[0]]=i+1
+            self.ngram_dict[pair[0]]=i+1
 
     def bag_of_words(self,corpus):
     
         bow = list()
-        ngram=max([len(gram) for gram in self.ngram_index.keys()])
+        ngram=max([len(gram) for gram in self.ngram_dict.keys()])
         for document in corpus:
-            doc_gram = [0]*len(self.ngram_index)
+            doc_gram = [0]*len(self.ngram_dict)
             for gram in range(1,ngram+1):
                 if gram == 1:
                     skip = 1
@@ -67,18 +62,19 @@ class NGramTokenizer(object):
                     skip = self.skipgram
                 for char_index in range(0,len(document)-gram+1,skip):
                     current_gram = document[char_index:char_index+gram]
-                    print(current_gram)
-                    if current_gram in self.ngram_index.keys():
-                        doc_gram[self.ngram_index[current_gram]-1] += 1
+                    if current_gram in self.ngram_dict.keys():
+                        doc_gram[self.ngram_dict[current_gram]-1] += 1
             bow.append(doc_gram)
         return bow
 
-def text_file_opener(file_name):
-    with open(os.getcwd() + file_name,'r',encoding='utf-8') as f:
-        return f.read().replace('\n',' ')
+    def ngram_index(self):
+        
+        if not self.ngram_dict:
+            raise Exception("No available index. Please fit corpus with method text_fitting.")
+            
+        for key in self.ngram_dict.keys():
+            self.ngram_dict[key] -= 1
+        return self.ngram_dict
+        
+        
 
-tokenizer = NGramTokenizer(15,2)
-texts = [text_file_opener(r'\teste.txt'),text_file_opener(r'\teste2.txt')]
-tokenizer.text_fitting(texts)
-
-BoW = tokenizer.bag_of_words(texts)
